0100a50c7b62f233ad55043341f54636686d5cd5
Gustavo Silva <contatogustavosilva017@outlook.com>
2024-11-01 06:33:46 -0300

    feat(pdfs) add new module
    - Covered advanced CSS [ Part 01 ] concepts.

--- gerar_pdf.py
+++ gerar_pdf.py
@@ -1,10 +1,6 @@
 from fpdf import FPDF
 import re
 
-def remover_caracteres_especiais(texto):
-    texto = texto.replace("—", "-").replace("“", '"').replace("”", '"').replace("‘", "'").replace("’", "'")
-    return texto
-
 def criar_pdf(titulo_pdf, conteudo):
     pdf = FPDF()
     pdf.set_auto_page_break(auto=True, margin=15)
@@ -18,7 +14,7 @@ def criar_pdf(titulo_pdf, conteudo):
     pdf.ln(10)
     
     pdf.set_font('Arial', '', 12)
-    conteudo = remover_caracteres_especiais(conteudo)
+    conteudo = conteudo.replace("—", "-").replace("“", '"').replace("”", '"').replace("‘", "'").replace("’", "'")
     pdf.multi_cell(0, 10, conteudo)
     pdf.ln(5)
     pdf.line(10, pdf.get_y(), 200, pdf.get_y())
@@ -26,13 +22,21 @@ def criar_pdf(titulo_pdf, conteudo):
     nome_arquivo = re.sub(r'[\\/*?:"<>|]', "", titulo_pdf.replace(" ", "_")) + ".pdf"
     pdf.output(nome_arquivo)
 
-titulo = "Incorporando Vídeos com iframe"
-conteudo = """Incorporando Vídeos com iframe
-A tag <iframe> é usada para inserir outra página HTML dentro de uma página principal, funcionando como uma "janela" que mostra o conteúdo de um site externo. Um dos usos mais comuns do <iframe> é incorporar vídeos de plataformas como YouTube e Vimeo, tornando o processo de inclusão de mídia mais leve e prático.
-Exemplo:
-<iframe width="560" height="315" src="https://www.youtube.com/embed/NcGnLdUPnpo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+titulo = "Variáveis no CSS"
+conteudo = """Variáveis no CSS
+As variáveis CSS são uma adição recente e poderosa que permite definir valores reutilizáveis no estilo. Elas facilitam a manutenção e permitem alterar valores de maneira centralizada.
+Definindo variáveis:
 
-Neste exemplo, o vídeo é carregado diretamente do YouTube e a largura e altura do quadro podem ser ajustadas conforme necessário. O atributo allowfullscreen permite que o usuário expanda o vídeo para tela cheia.
-O <iframe> é ideal para vídeos por não exigir o carregamento direto no servidor, economizando espaço e recursos de banda."""
+:root {
+  --cor-principal: #3498db;
+  --tamanho-texto: 16px;
+}
+
+h1 {
+  color: var(--cor-principal);
+  font-size: var(--tamanho-texto);
+}
+
+As variáveis podem ser redefinidas em diferentes contextos, permitindo criar temas dinâmicos. O uso de variáveis facilita a atualização de estilos e garante consistência visual em grandes projetos."""
 
 criar_pdf(titulo, conteudo)
